87313a167885790d96ad34c956e498ecb43acca9
DESKTOP-60C26NI\vincent <manuelal180@hotmail.com>
2023-03-04 20:29:37 +0100

    Declaración de variables

---
+++ Tutorial/src/variables/Declaracion.java
@@ -0,0 +1,15 @@
+package variables;
+
+public class Declaracion {
+
+	public static void main(String[] args) {
+		// https://docs.oracle.com/javase/tutorial/java/nutsandbolts/variables.html
+		
+		// <Tipo> <Nombre>; esta es una declaración de variable
+		// <Tipo> <Nombre> = <Valor>; esta es una declaración de variable con asignación de valor
+		int variable = 0;
+		
+		System.out.println("Esto es una variable: " + variable);
+	}
+
+}
