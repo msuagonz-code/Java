7b9dc566da800a805033f0709a09ee0a5e92019c
msuagonz <manuelal180@hotmail.com>
2023-03-17 20:23:25 +0100

    Switch

---
+++ Tutorial/src/controlFlow/Switch.java
@@ -0,0 +1,14 @@
+package controlFlow;
+
+/*
+ * https://docs.oracle.com/javase/tutorial/java/nutsandbolts/switch.html
+ * 
+ * */
+
+public class Switch {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
