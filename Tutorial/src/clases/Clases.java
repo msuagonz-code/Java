e6afaf757d32baa4b27333dac558981a21d2c214
Sam <manuelal180@hotmail.com>
2023-03-19 19:06:04 +0100

    Declaracion de clases

---
+++ Tutorial/src/clases/Clases.java
@@ -0,0 +1,16 @@
+package clases;
+
+/*
+ * https://docs.oracle.com/javase/tutorial/java/javaOO/classes.html
+ * https://docs.oracle.com/javase/tutorial/java/javaOO/classdecl.html
+ * 
+ * */
+
+public class Clases {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
