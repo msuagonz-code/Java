be2c0c7d848b12a80fe3e7872fb9fa5c6875fd7e
Sam <manuelal180@hotmail.com>
2023-04-02 10:15:10 +0200

    Metodos

---
+++ Tutorial/src/clases/Metodos.java
@@ -0,0 +1,25 @@
+package clases;
+
+/*
+ * https://docs.oracle.com/javase/tutorial/java/javaOO/methods.html
+ * */
+
+public class Metodos {
+
+	public static void main(String[] args) {
+		/* 
+		 * Declaración de metodos
+		 * Generalmente la declaración de metodos tienen los siguientes componentes
+		 * declarados en orden:
+		 * 	+ Modificadores.
+		 * 	+ Tipos de retorno.
+		 * 	+ Nombre del metodo.
+		 * 	+ Parametros listados entre parentesis ().
+		 * 	+ Lista de excepciones.
+		 * 	+ El cuerpo del metodo entre llaves {}.
+		 * 
+		 * Sobrecarga de metodos
+		 * */
+	}
+
+}
