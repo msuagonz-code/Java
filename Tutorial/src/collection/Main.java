33b718b055b05c3406323d7f40455908c421f6e1
DESKTOP-RA0OT43\Sam <manuelal180@hotmail.com>
2026-03-20 16:02:48 +0100

    Ejemplo prácitoc de la interfac collection

--- Tutorial/src/collection/Main.java
+++ Tutorial/src/collection/Main.java
@@ -2,22 +2,13 @@ package collection;
 
 import java.util.Iterator;
 
+import collection.collections.CollectionsDemo;
 import collection.iterable.GenericList;
 
 public class Main {
 
 	public static void main(String[] args) {
-		GenericList list = new GenericList<String>();
-
-		list.add("a");
-		list.add("b");
-		list.add("c");
-		list.add("d");
-		
-		for(Object item : list) {
-				System.out.println(item);
-		}
-		
+		CollectionsDemo.show(); 
 	}
 
 }
